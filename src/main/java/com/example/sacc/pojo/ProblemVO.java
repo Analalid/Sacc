550f8506446726407b621347a099a2c6e1cd0f2b
happyScar <b21032130@njupt.edu.cn>
2022-08-30 11:24:35 +0800

    选项更新

--- src/main/java/com/example/sacc/pojo/ProblemVO.java
+++ src/main/java/com/example/sacc/pojo/ProblemVO.java
@@ -12,19 +12,19 @@ public class ProblemVO {
     Long id;
     Integer type;
     String title;
-    String opt1;
-    String opt2;
-    String opt3;
-    String opt4;
+    String A;
+    String B;
+    String C;
+    String D;
     String content;
 
     public ProblemVO(Problem problem) {
         this.id=problem.getId();
         this.type=problem.getType();
         this.title=problem.getTitle();
-        this.opt1=problem.getA();
-        this.opt2=problem.getB();
-        this.opt3=problem.getC();
-        this.opt4=problem.getD();
+        this.A=problem.getA();
+        this.B=problem.getB();
+        this.C=problem.getC();
+        this.D=problem.getD();
     }
 }
